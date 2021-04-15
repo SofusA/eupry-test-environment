76707ac8d017b0f7cf2d921f9ca75bc7a15fb1ce
soa <soa@eupry.com>
2021-04-15 12:44:16 +0000

    i dont know maaan

--- client/src/static/url.js
+++ client/src/static/url.js
@@ -1,4 +1,4 @@
-let url = "http://35.231.114.250:1880";
+//let url = "http://35.231.114.250:1880";
 // let url = "http://127.0.0.1:1880";
-// let url = "https://test2020.eupry.com";
+let url = "https://betatest.eupry.com";
 export { url };
